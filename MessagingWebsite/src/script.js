d06543dcc60db182b1e62e04c7e430b2f3fea2d9
Malcolm McDonald <malcolmemcdonald@gmail.com>
2023-09-25 11:00:02 -0700

    finishing the messaging and login systems

--- MessagingWebsite/src/script.js
+++ MessagingWebsite/src/script.js
@@ -2,10 +2,13 @@ let userName;
 let email;
 let password;
 let user;
+let tempEmail
+let tempPassword
+let tempUserName;
 // Import the functions you need from the SDKs you need
 import { initializeApp, onLog } from "firebase/app";
 import { getFirestore, collection, addDoc, onSnapshot, query, doc, orderBy, serverTimestamp } from "firebase/firestore";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, updateProfile } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -30,8 +33,9 @@ const db = getFirestore()
 
 const colRef = collection(db, "Messages")
 
+const logoutButton = document.getElementById('logoutBtn');
+const messageSubmission = document.querySelector(".message-submission")
 
-// const messageSubmission = document.querySelector("message-submission")
 
 //**************************show login/sign up forms */
 const showForm_container = document.querySelector('.form-container')
@@ -112,30 +116,46 @@ document.addEventListener('DOMContentLoaded', function () {
     // const signupForm = document.querySelector(".SignupForm");
     signUpForm.addEventListener("submit", (e) => {
         e.preventDefault()
-        userName = signUpForm.signupUsername.value
-        email = signUpForm.signupEmail.value
-        password = signUpForm.signupPassword.value
+        tempUserName = signUpForm.signupUsername.value
+        tempEmail = signUpForm.signupEmail.value
+        tempPassword = signUpForm.signupPassword.value
 
-        createUserWithEmailAndPassword(auth, email, password)
+        createUserWithEmailAndPassword(auth, tempEmail, tempPassword)
             .then((userCred) => {
                 user = userCred.user
-                console.log("user created", userCred)
+                console.log("user created", userCred.user)
+                return updateProfile(auth.currentUser, {
+                    displayName: tempUserName,
+                })
+                    .then(() => {
+                        setUserInfo(userCred)
+                    })
+                    .catch((error) => {
+                        console.log(error.message)
+                    })
+
             })
             .catch((error) => {
                 console.log(error.message)
             })
+
     });
 
 
     loginForm.addEventListener('submit', (e) => {
         e.preventDefault();
-        // userName = signUpForm.signupUsername.value
-        email = signUpForm.signupEmail.value
-        password = signUpForm.signupPassword.value
+        tempUserName = loginForm.loginUsername.value
+        tempEmail = loginForm.loginEmail.value
+        tempPassword = loginForm.loginPassword.value
 
-        signInWithEmailAndPassword(auth, password, email)
+        signInWithEmailAndPassword(auth, tempEmail, tempPassword)
             .then((userCred) => {
                 user = userCred.user
+                console.log(`logged in as: ${user}`)
+                setUserInfo(userCred)
+            })
+            .catch((error) => {
+                console.log(error.message)
             })
     })
 })
@@ -148,7 +168,7 @@ onSnapshot(messageQ, (snapshot) => {
             let message = { ...change.doc.data(), id: change.doc.id }
             var container = document.getElementById('messagesArrayContainer');
             var div = document.createElement('div');
-            div.textContent = `${message.user}: ${message.content}`;
+            div.textContent = `${message.userName}: ${message.content}`;
             container.appendChild(div);
             window.scrollTo(0, document.body.scrollHeight);  // Scroll to bottom
 
@@ -157,3 +177,72 @@ onSnapshot(messageQ, (snapshot) => {
 })
 
 
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // *User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid;
+        setUserInfo(user)
+        console.log(`logged in as: `, user)
+        //hides all the unnecessary buttons/forms
+        showSignUpFormBtn.classList.add('hidden');
+        showLoginFormBtn.classList.add('hidden');
+        signUpForm.classList.add('hidden');
+        loginForm.classList.add('hidden');
+        showForm_container.classList.add('hidden');
+        //reveals the logout button
+        logoutButton.classList.remove('hidden');
+        //reveals the message form
+        messageSubmission.classList.remove('hidden');
+
+        // message submission listener
+        const submitMessageBtn = document.getElementById('submitMessageBtn')
+        const message = document.getElementById('message')
+        submitMessageBtn.addEventListener('click', () => {
+            addDoc(colRef, { // adds the message to the message document
+                content: message.value,
+                userName: user.displayName,
+                userEmail: user.email,
+                messageCreated: serverTimestamp()
+            })
+                .then(() => {
+                })
+            message.reset()
+            console.log("reset textarea")
+
+        })
+
+
+
+        // ...
+    } else {
+        // *User is signed out
+        //reveals the login/signup buttons
+        showSignUpFormBtn.classList.remove('hidden');
+        showLoginFormBtn.classList.remove('hidden');
+        //hides the logout buttons
+        logoutButton.classList.add('hidden');
+        //hides the message submission form
+        messageSubmission.classList.add('hidden');
+
+    }
+});
+
+logoutButton.addEventListener('click', () => {
+
+    signOut(auth)
+        .then(() => {
+            console.log("User logged out")
+            loginForm.reset()
+        })
+        .catch((err) => {
+            console.log(err.message)
+        })
+})
+
+
+function setUserInfo(userCred) {
+    email = userCred.email;
+    userName = userCred.isplayName;
+}
